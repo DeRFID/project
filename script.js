a9ac76feab8b3014b4be1dc5e1233794f5a5e42d
DeRFID <t.aset2007@gmail.com>
2025-05-02 08:27:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function modalWindows(){
     const modal = document.getElementById('modal')
     const opener = document.getElementsByClassName('button')
@@ -41,62 +40,7 @@ init()
     console.log("balumba");
     
 }
-
-
-
-const arrowLeft = document.querySelector('.arrow-left');
-const arrowRight = document.querySelector('.arrow-right');
-const slides = document.querySelectorAll('.slide');
-const slider = document.querySelector('.slider');
-let currentSlideIndex = 0;
-
-function showSlide(){
-    const slideWidth = slides[0].clientWidth;
-    slider.style.transform = `translateX(-${currentSlideIndex * slideWidth}px)`;
-}
-
-function nextSlide(){
-    currentSlideIndex++;
-    if(currentSlideIndex >= slides.length){
-        currentSlideIndex = 0;
-    }
-    showSlide();
-}
-
-function previousSlide(){
-    currentSlideIndex--;
-    if(currentSlideIndex < 0){
-        currentSlideIndex = slides.length - 1;
-    }
-    showSlide();
-}
-
-arrowRight.addEventListener('click', nextSlide);
-arrowLeft.addEventListener('click', previousSlide);
-
-
-
-const benefitsSlides = document.querySelectorAll('.slide__benefits');
-const benefitsBtns = document.querySelectorAll('.benefits__bar--size');
-
-function handleButtonClick(index) {
-    for (let i = 0; i < benefitsSlides.length; i++) {
-        benefitsSlides[i].classList.remove('block');
-    }
-
-    benefitsSlides[index].classList.add('block');
-}
-
-for (let i = 0; i < benefitsBtns.length; i++) {
-    benefitsBtns[i].addEventListener('click', function() {
-        handleButtonClick(i);
-    });
-}
-
-
-
 function copyNumber() {
-    // Создаём временный input
     var tempInput = document.createElement("input");
     tempInput.value = "7 777 777 7777";
     document.body.appendChild(tempInput);
@@ -104,11 +48,11 @@ function copyNumber() {
     document.execCommand("copy");
     document.body.removeChild(tempInput);
   
-    // Показываем кастомный алерт
+
     var alertBox = document.getElementById("customAlert");
     alertBox.style.display = "block";
   
-    // Скрываем алерт через 3 секунды
+
     setTimeout(function() {
       alertBox.style.display = "none";
     }, 3000);
@@ -122,4 +66,6 @@ function showConfirmation() {
       popup.style.display = "none";
     }, 3000);
   }
+
+
   
